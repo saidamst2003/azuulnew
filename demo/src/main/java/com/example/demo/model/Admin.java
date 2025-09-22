eeab7fabffbea7597f539b05876793a397a9166d
saidamst2003 <saida2003mst@gmail.com>
2025-09-22 13:05:27 +0100

    j'ai creer model de admin

---
+++ demo/src/main/java/com/example/demo/model/Admin.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "admins")
+@DiscriminatorValue("ADMIN")
+public class Admin extends Utilisateur {
+
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference("admin-ateliers")
+    private List<Atelier> ateliers = new ArrayList<>();
+
+    // Constructeur vide
+    public Admin() {}
+
+    public Admin(@NotBlank(message = "full name is required") String s, @NotBlank(message = "email is required") @Email(message = "please enter a valid email") String email, String encode) {
+    }
+
+    @Override
+    public void seConnecter() {
+
+    }
+
+    // Constructeur pour la création (sans id)
+    public Admin(String fullName, String email, String password, Role role) {
+        super(null, fullName, email, password, role);
+
+    }
+
+
+}
