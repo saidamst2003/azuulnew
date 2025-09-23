7196bd6c51296b49ee7c3eb62e3a8d9def780599
saidamst2003 <saida2003mst@gmail.com>
2025-09-23 17:49:10 +0100

    j'ai creer  function creation atelier dans service

--- demo/src/main/java/com/example/demo/service/AtelierService.java
+++ demo/src/main/java/com/example/demo/service/AtelierService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.AtelierDTO;
+import com.example.demo.mapper.AtelierMapper;
 import com.example.demo.model.Atelier;
 import com.example.demo.repository.AtelierRepo;
 import org.springframework.stereotype.Service;
@@ -9,15 +10,17 @@ import org.springframework.stereotype.Service;
 public class AtelierService {
 
     private final AtelierRepo atelierRepo;
+    private final AtelierMapper atelierMapper;
 
-    public AtelierService(AtelierRepo atelierRepo){
+    public AtelierService(AtelierRepo atelierRepo, AtelierMapper atelierMapper){
         this.atelierRepo = atelierRepo;
+        this.atelierMapper = atelierMapper;
     }
 
     //creation atelier
 
     public AtelierDTO createAtelier(Atelier atelier){
         Atelier saved =atelierRepo.save(atelier);
-        return toDTO(saved)
+        return atelierMapper.toDTO(saved);
     }
 }
