b721ac2aba74ba0308c32d746b176ac4e256fc3f
saidamst2003 <saida2003mst@gmail.com>
2025-09-23 17:52:34 +0100

    j'ai injecter service atelier dans controller atelier

--- demo/src/main/java/com/example/demo/controller/AtelierController.java
+++ demo/src/main/java/com/example/demo/controller/AtelierController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.AtelierService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AtelierController {
+    private final AtelierService atelierService;
+
+    public AtelierController(AtelierService atelierService){
+        this.atelierService =atelierService;
+    }
+
 }
