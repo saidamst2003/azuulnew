b2fab4bfe3f96838945f01eb289056bb717fb76c
saidamst2003 <saida2003mst@gmail.com>
2025-09-23 17:05:33 +0100

    j'ai fais l'injection de repo de atelier dans service atelier

--- demo/src/main/java/com/example/demo/service/AtelierService.java
+++ demo/src/main/java/com/example/demo/service/AtelierService.java
@@ -1,5 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Atelier;
+import com.example.demo.repository.AtelierRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AtelierService {
+
+    private final AtelierRepo atelierRepo;
+
+    public AtelierService(AtelierRepo atelierRepo){
+        this.atelierRepo = atelierRepo;
+    }
 
 }
