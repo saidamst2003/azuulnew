e1400f590e44fd4ca52b8c5f8162892a4bae6656
saidamst2003 <saida2003mst@gmail.com>
2025-10-05 20:12:19 +0100

    j'ai creer register avec condition de  role

--- demo/src/main/java/com/example/demo/service/Authservice.java
+++ demo/src/main/java/com/example/demo/service/Authservice.java
@@ -17,4 +17,38 @@ public class Authservice {
         this.passwordEncoder = passwordEncoder;
     }
 
+//creer registration avec role
+    public Utilisateur registerUser(RegisterDTO registerDTO, String role) {
+        Utilisateur newUser;
+
+        if (role.equalsIgnoreCase("admin")) {
+            newUser = new Admin(
+                    registerDTO.fullName(),
+                    registerDTO.email(),
+                    passwordEncoder.encode(registerDTO.password()),
+                    Role.ADMIN
+            );
+        } else if (role.equalsIgnoreCase("client")) {
+            newUser = new Client(
+                    registerDTO.fullName(),
+                    registerDTO.email(),
+                    passwordEncoder.encode(registerDTO.password()),
+                    Role.CLIENT
+            );
+        } else if (role.equalsIgnoreCase("coach")) {
+            // Ici pas de spécialité fournie → valeur fixe par défaut
+            newUser = new Coach(
+                    registerDTO.fullName(),
+                    registerDTO.email(),
+                    passwordEncoder.encode(registerDTO.password()),
+                    Role.COACH,
+                    "Aucune spécialité"
+            );
+        } else {
+            throw new IllegalArgumentException("Rôle non reconnu : " + role);
+        }
+
+        return userRepository.save(newUser);
+    }
+
 }
