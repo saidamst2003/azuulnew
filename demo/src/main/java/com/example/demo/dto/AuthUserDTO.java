70955676ffa24ad150c408a53119edf7e022fcfb
saidamst2003 <saida2003mst@gmail.com>
2025-10-05 20:14:54 +0100

    j'ai creer AuthUserDTO

---
+++ demo/src/main/java/com/example/demo/dto/AuthUserDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+
+import com.example.demo.model.Role;
+
+public record AuthUserDTO(
+        Long id,
+        String fullName,
+        String email,
+        Role role
+) {
+}
